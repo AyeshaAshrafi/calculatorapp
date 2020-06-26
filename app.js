ef716191a70ee156f7292ccee9a2a30b3b7ab62f
AyeshaAshrafi <brilliant.ashi100@gmail.com>
2020-06-26 22:52:32 +0500

    ready to commit

---
+++ app.js
@@ -0,0 +1,19 @@
+
+
+// CALCULATOR FUNCTIONALITY
+
+function getNumber(num){
+var answer = document.getElementById("answer");
+answer.value += num;
+}
+
+function clearResult(){
+    var answer = document.getElementById("answer");
+    answer.value = ""
+}
+
+function getResult(){
+    var answer = document.getElementById("answer");
+    answer.value = eval(answer.value)
+}
+
